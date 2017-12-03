2a509721e1a9d089879f8952be7dc3019b7dd64c
Juvenal Yescas <juvenal@mail.com>
2017-12-02 22:37:35 -0600

    Add style agregar.php

--- agregar.php
+++ agregar.php
@@ -27,48 +27,56 @@ if($_REQUEST['registrar'] == "Registrar"){
 }
 ?>
 <!DOCTYPE html>
+<html>
 <html lang="es-ES">
-
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <head>
-  <meta charset="UTF-8">
-  <title>Hospital</title>
-  <script type="text/javascript">
-function showFileName() {
-   var fil = document.getElementById("myFile");
-   alert(fil.value);
-}
-</script>
-
+<title>Empenios Oaxaca</title>
+    <script type="text/javascript">
+        function showFileName() {
+           var fil = document.getElementById("myFile");
+           alert(fil.value);
+        }
+    </script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </head>
-<body>
+<body class="container">
+    <div class="col-lg-4 col-offset-6 centered">
+        <form method="POST" action="agregar.php">
+          <div class="form-group">
+            <label for="InputProducto">Nombre</label>
+            <input type="text" name="nombre" class="form-control" id="InputProducto" aria-describedby="textHelp" placeholder="Nombre de producto">
+            <small id="textHelp" class="form-text text-muted">Un nombre corto y simple.</small>
+          </div>
 
-      <div class="form-group">
-        <label>Nombre</label><br>
-        <input type="text" name="nombre" maxlength="60" placeholder="Nombre">
-      </div>
-      <div class="form-group">
-        <label>Descripcion</label><br>
-        <input type="text" name="descripcion" maxlength="80" placeholder="Descripción">
-      </div>
-      <div class="form-group">
-        <label>Marca</label><br>
-        <input type="text" name="nombre" maxlength="60" placeholder="Nombre">
-      </div>
-      <div class="form-group">
-        <label>Precio</label><br>
-        <input type="text" name="descripcion" maxlength="80" placeholder="Descripción">
-      </div>
+          <div class="form-group">
+            <label for="InputProducto">Marca</label>
+            <input type="text" name="marca" class="form-control" id="InputProducto" aria-describedby="textHelp" placeholder="HP,Apple">
+          </div>
 
-<form method="POST" action="agregar.php" enctype="multipart/form-data">
-    <p>File to upload : <input type ="file" name = "UploadFileName"></p><br />
-    <input type = "submit" name = "Submit" value = "Press THIS to upload">
-</form>
+          <div class="form-group">
+            <label for="InputProducto">Precio</label>
+            <input type="text" name="precio" class="form-control" id="InputProducto" aria-describedby="textHelp" placeholder="120.0">
+          </div>
 
-      <div class="form-group col-md-6">
-            <input type="submit" name="registrar" value="Registrar">
-      </div>
-    </form>
-  </div>
+          <div class="form-group">
+            <label for="TextAreaproducto">Descripción</label>
+            <textarea name="descripcion" class="form-control" id="TextAreaproducto" rows="3"></textarea>
+          </div>
+
+          <div class="form-group">
+            <label for="fileProduct">Seleccionar imagen</label>
+            <input type="file" name="rutaImagen" class="form-control-file" id="fileProduct">
+          </div>
+
+          <button type="submit" name="registrar" class="btn btn-primary">Registrar</button>
+        </form>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
 </body>
-
 </html>
