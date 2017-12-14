2e1a412b038f6095a87c81b9b3cb74efbd38a0e3
EEllisonJM <ellison_jm@hotmail.com>
2017-12-13 20:34:26 -0600

    Busqueda mejorada

--- index.php
+++ index.php
@@ -1,34 +1,29 @@
-<?php 
+<?php
 error_reporting(E_ALL ^ E_NOTICE);
 include("./db/configuracion.php");
 include("encabezado.php");
 include("categoria.php");
+
 /*Conectar con el servidor*/
 $conexion = new mysqli($servername, $username, $password, $dbname);
 /*Revisar conexion*/
 if ($conexion->connect_error) {
-  die("Connection failed: " . $conexion->connect_error);
+    die("Connection failed: " . $conexion->connect_error);
 }
 ?>
-<html>  
-
+<html><!-- Inicio HTML-->
 <?php
 if (!empty($_GET['search'])) {
-  $nombre = $_GET['search'];
-  $sql    = "SELECT Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto
-  WHERE Nombre='$nombre';";
-  $result = $conexion->query($sql);
+    $nombre = $_GET['search'];
+    $sql    = "SELECT Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto
+  WHERE Nombre LIKE '$nombre%';";
+    $result = $conexion->query($sql);
+    include("mostrarProductos.php");
+}else {
+    $sql    = "SELECT Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto";
+    $result = $conexion->query($sql);
+    include("mostrarProductos.php");
+}
 ?>
-<?php
-include("mostrarProductos.php");
-}else{
-  $sql    = "SELECT Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto";
-  $result = $conexion->query($sql);
-?>
-
-<html>  
-<?php
-include("mostrarProductos.php");
-}?>
 <footer>Todos los derechos reservados</footer>
 </html> 
