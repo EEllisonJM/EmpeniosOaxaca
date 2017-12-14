5a3a0b1548610726a34d6a05657512eca4b69b00
EEllisonJM <ellison_jm@hotmail.com>
2017-12-14 15:20:37 -0600

    Revert "Enviando id de producto"
    This reverts commit 565863d8470a31e146d710bda6eca8b3fdb0a71e.

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ error_reporting(E_ALL ^ E_NOTICE);
 include("./db/configuracion.php");
 include("encabezado.php");
 include("categoria.php");
-
 /*Conectar con el servidor*/
 $conexion = new mysqli($servername, $username, $password, $dbname);
 /*Revisar conexion*/
@@ -13,14 +12,23 @@ if ($conexion->connect_error) {
 ?>
 <html><!-- Inicio HTML-->
 <?php
-if (!empty($_GET['search'])) {
+if (!empty($_POST['reservar'])) {/*RESERVAR PRODUCTO*/
+  //$nombre = $_GET['search'];
+    //$sql    = "SELECT idProducto, Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto
+  //WHERE Nombre LIKE '$nombre%' and reservado=0;";
+    //$result = $conexion->query($sql);
+  echo "Quiero pasar por aqui el nombre del producto, y asi mostrarlo en reservar.php";
+  include("reservar.php");
+}else{
+
+if (!empty($_GET['search'])) {/*MOSTRAR PRODUCTO A BUSCAR*/
     $nombre = $_GET['search'];
-    $sql    = "SELECT Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto
-  WHERE Nombre LIKE '$nombre%';";
+    $sql    = "SELECT idProducto, Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto
+  WHERE Nombre LIKE '$nombre%' and reservado=0;";
     $result = $conexion->query($sql);
     include("mostrarProductos.php");
 }else{
-if (
+if (/*SELECCIONAR CATEGORIA A MOSTRAR*/
   (!empty($_GET['categoria']=="Computacion")) ||
   (!empty($_GET['categoria']=="Consolas")) ||
   (!empty($_GET['categoria']=="Celulares")) ||
@@ -29,14 +37,15 @@ if (
   (!empty($_GET['categoria']=="Televisores"))
 ) {  
   $categoria=$_GET['categoria'];
-    $sql    = "SELECT Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto
-  WHERE categoria='$categoria'";
+    $sql    = "SELECT idProducto, Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto
+  WHERE categoria='$categoria' and reservado=0";
     $result = $conexion->query($sql);
     include("mostrarProductos.php");
-}else {
-    $sql    = "SELECT idProducto,Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto";
+}else {/*MOSTRAR PRODUCTOS*/
+    $sql    = "SELECT idProducto, Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto WHERE reservado=0" ;
     $result = $conexion->query($sql);
     include("mostrarProductos.php");
+}
 }
 }
 ?>
--- mostrarProductos.php
+++ mostrarProductos.php
@@ -3,10 +3,10 @@ if ($result->num_rows > 0) {
     /*Mostrar los datos*/
     while ($row = $result->fetch_assoc()) {
 ?>
- <article>
-    <div class="gallery">
-      <a target="_blank"
-         href="<?php
+<article>
+  <div class="gallery">
+    <a target="_blank"
+    href="<?php
         echo $row["rutaImagen"]; 
 ?>">
         <img src="<?php
@@ -30,18 +30,16 @@ if ($result->num_rows > 0) {
      </div>
       <div class="precio">
         <?php
-        echo $row["Precio"];
+        echo "$".$row["Precio"];
 ?>
-
       </div>
       <div class="reservar">
-        <form action="reservar.php" method="POST">
-            <input type="text" hidden name="id" value='<?php echo $row['idProducto']; ?>'>
-    <input type="submit" value="Reservar producto" />
-</form>
+        <form action="index.php" method="post">
+          <input type="submit" value="Reservar producto" name="reservar" />
+        </form>
       </div>
     </div>
-  </article>
+</article>
   <?php
     }
 } else {
