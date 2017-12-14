565863d8470a31e146d710bda6eca8b3fdb0a71e
Juvenal Yescas <juvenal@mail.com>
2017-12-14 14:51:08 -0600

    Enviando id de producto

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ error_reporting(E_ALL ^ E_NOTICE);
 include("./db/configuracion.php");
 include("encabezado.php");
 include("categoria.php");
+
 /*Conectar con el servidor*/
 $conexion = new mysqli($servername, $username, $password, $dbname);
 /*Revisar conexion*/
@@ -12,23 +13,14 @@ if ($conexion->connect_error) {
 ?>
 <html><!-- Inicio HTML-->
 <?php
-if (!empty($_POST['reservar'])) {/*RESERVAR PRODUCTO*/
-  //$nombre = $_GET['search'];
-    //$sql    = "SELECT idProducto, Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto
-  //WHERE Nombre LIKE '$nombre%' and reservado=0;";
-    //$result = $conexion->query($sql);
-  echo "Quiero pasar por aqui el nombre del producto, y asi mostrarlo en reservar.php";
-  include("reservar.php");
-}else{
-
-if (!empty($_GET['search'])) {/*MOSTRAR PRODUCTO A BUSCAR*/
+if (!empty($_GET['search'])) {
     $nombre = $_GET['search'];
-    $sql    = "SELECT idProducto, Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto
-  WHERE Nombre LIKE '$nombre%' and reservado=0;";
+    $sql    = "SELECT Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto
+  WHERE Nombre LIKE '$nombre%';";
     $result = $conexion->query($sql);
     include("mostrarProductos.php");
 }else{
-if (/*SELECCIONAR CATEGORIA A MOSTRAR*/
+if (
   (!empty($_GET['categoria']=="Computacion")) ||
   (!empty($_GET['categoria']=="Consolas")) ||
   (!empty($_GET['categoria']=="Celulares")) ||
@@ -37,15 +29,14 @@ if (/*SELECCIONAR CATEGORIA A MOSTRAR*/
   (!empty($_GET['categoria']=="Televisores"))
 ) {  
   $categoria=$_GET['categoria'];
-    $sql    = "SELECT idProducto, Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto
-  WHERE categoria='$categoria' and reservado=0";
+    $sql    = "SELECT Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto
+  WHERE categoria='$categoria'";
     $result = $conexion->query($sql);
     include("mostrarProductos.php");
-}else {/*MOSTRAR PRODUCTOS*/
-    $sql    = "SELECT idProducto, Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto WHERE reservado=0" ;
+}else {
+    $sql    = "SELECT idProducto,Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto";
     $result = $conexion->query($sql);
     include("mostrarProductos.php");
-}
 }
 }
 ?>
