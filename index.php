f8afb1a9a486c6281a5180c18fa1ac58ae2c5272
EEllisonJM <ellison_jm@hotmail.com>
2017-12-14 15:20:17 -0600

    Revert "Revert "; agregado" - ocupare parte del codigo de juve"
    This reverts commit dd65253b548f2c19e558aa6e137421f6d0bb2b6d.

--- index.php
+++ index.php
@@ -13,20 +13,7 @@ if ($conexion->connect_error) {
 ?>
 <html><!-- Inicio HTML-->
 <?php
-<<<<<<< HEAD
 if (!empty($_GET['search'])) {
-=======
-if (!empty($_POST['reservar'])) {/*RESERVAR PRODUCTO*/
-  //$nombre = $_GET['search'];
-    //$sql    = "SELECT idProducto, Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto
-  //WHERE Nombre LIKE '$nombre%' and reservado=0;";
-    //$result = $conexion->query($sql);
-  echo "Quiero pasar por aqui el nombre del producto, y asi mostrarlo en reservar.php"
-  include("reservar.php");
-}else{
-
-if (!empty($_GET['search'])) {/*MOSTRAR PRODUCTO A BUSCAR*/
->>>>>>> parent of 45fd1dd... ; agregado
     $nombre = $_GET['search'];
     $sql    = "SELECT Nombre, Descripcion, Marca, Precio,rutaImagen,categoria FROM Producto
   WHERE Nombre LIKE '$nombre%';";
