732c5f116783b0473f46609a7928f9b98d34e6ba
EEllisonJM <ellison_jm@hotmail.com>
2017-12-15 10:45:32 -0600

    Boton bonito Reservar

--- mostrarProductos.php
+++ mostrarProductos.php
@@ -13,30 +13,30 @@ if ($result->num_rows > 0) {
         echo $row["rutaImagen"];
 ?>"  />
       </a>
-      <div class="nombre">
+      <div class="datosProducto">
         <?php
         echo $row["nombre"];
 ?>
      </div>
-      <div class="marca">
+      <div class="datosProducto">
         <?php
         echo $row["marca"];
 ?>
      </div>
-      <div class="descripcion">
+      <div class="datosProducto">
         <?php
         echo $row["descripcion"];
 ?>
      </div>
-      <div class="precio">
+      <div class="datosProducto">
         <?php
         echo "$".$row["precio"];
 ?>
       </div>
       <div class="reservar">
         <form action="index.php" method="POST">
-            <input type="text" hidden name="reservargetID" value='<?php echo $row['idProducto']; ?>'>
-            <input type="submit" value="Reservar producto" />
+            <input type="text" hidden="hidden" name="reservargetID" value='<?php echo $row['idProducto']; ?>'>
+            <input type="submit" value="Reservar producto" name="btnReservar" id="btnReservar" />
         </form>
       </div>
     </div>
